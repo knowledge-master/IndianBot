0e28adc9917cf3d9187f98ca40831753020b175c
knowledge-master <64637863+knowledge-master@users.noreply.github.com>
2020-05-05 12:23:00 +0530

    Update deploy.py

--- userbot/plugins/deploy.py
+++ userbot/plugins/deploy.py
@@ -35,7 +35,7 @@ async def _(event):
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (indianbhaiya/IndianBot)**",
+            "**Heroku Connecting To Latest Github Build (by @knowledge_masterr)**",
             "**Build started by user** **{DEFAULTUSER}**",
             "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
             "**Restarting Heroku Server...**",
@@ -44,8 +44,8 @@ async def _(event):
             "**Process exited with** `status 143`",
             "**Starting process with command** `python3 -m stdborg`",
             "**State changed from starting to up**",
-            "__INFO:IndianBot:Logged in as 557667062__",
-            "__INFO:IndianBot:Successfully loaded all plugins__",
+            "__INFO:K_M_BOT:Logged in as 557667062__",
+            "__INFO:K_M_BOT:Successfully loaded all plugins__",
             "**Build Succeeded**"
 
  ]
